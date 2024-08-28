9791321e8bf9f4d7b67447e5adbc5e778be86e5e
Santiago Betancur Duque <santiagobetancurd.contratista@comfama.com.co>
2024-08-28 18:06:11 -0500

    feat: add webpack related typeScript configurations to container

---
+++ container-app/src/@types/remote-modules.d.ts
@@ -0,0 +1,9 @@
+declare module "mfAngular14/Home" {
+	const mfAngular14: any;
+	export default mfAngular14;
+}
+
+declare module "mfAngular17/Login" {
+	const mfAngular17: any;
+	export default mfAngular17;
+}
--- container-app/src/App.tsx
+++ container-app/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { lazy, Suspense } from "react";
+
+const MfAngular14 = lazy(() => import("mfAngular14/Home"));
+const MfAngular17 = lazy(() => import("mfAngular17/Login"));
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div>
+			<h1>Contenedor Vite con Microfrontends de Angular</h1>
+			<Suspense fallback={<div>Cargando Angular 14...</div>}>
+				<MfAngular14 />
+			</Suspense>
+			<Suspense fallback={<div>Cargando Angular 17...</div>}>
+				<MfAngular17 />
+			</Suspense>
+		</div>
+	);
 }
 
-export default App
+export default App;
