40d94d563ac597be703b045f0220255fd7fb60ef
Santiago Betancur Duque <santiagobetancurd.contratista@comfama.com.co>
2024-08-29 12:11:20 -0500

    refactor: improve typeScript remote-modules declarations

--- shell-vite/src/@types/remote-modules.d.ts
+++ shell-vite/src/@types/remote-modules.d.ts
@@ -1,24 +1,19 @@
-declare module "mf_angular14/Module" {
+declare module "mf_angular14/*" {
 	const mf_angular14: any;
 	export default mf_angular14;
 }
 
-declare module "mfAngular17/Module" {
+declare module "mfAngular17/*" {
 	const mfAngular17: any;
 	export default mfAngular17;
 }
 
-declare module "products/ProductsIndex" {
+declare module "products/*" {
 	const products: any;
 	export default products;
 }
 
-declare module "remoteApp/RemoteComponent" {
-	const remoteApp: any;
-	export default remoteApp;
-}
-
-declare module "remoteApp/Input" {
+declare module "remoteApp/*" {
 	const remoteApp: any;
 	export default remoteApp;
 }
