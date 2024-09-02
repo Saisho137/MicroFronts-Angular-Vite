b36873f1f604753debea6546253cc201f74b207e
santiago <SantiagoBetancurD.contratista@comfama.com.co>
2024-09-02 17:18:45 -0500

    fix: added correct Provider implementation to clean-architecture mf

--- angular-mf-workspace/projects/clean-architecture/src/app/UI/main/main.module.ts
+++ angular-mf-workspace/projects/clean-architecture/src/app/UI/main/main.module.ts
@@ -4,12 +4,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main.component';
 import { AlbumApiServiceWithoutDelay } from '../../infraestructure/driven-adapter/album-api/album-api-withou-delay.service';
 import { AlbumGateway } from '../../domain/models/Album/gateway/album-gateway';
+import { GetAlbumUseCases } from '../../domain/usecase/get-album-use-case';
+// import { AlbumApiService } from './infraestructure/driven-adapter/album-api/album-api.service';
 
 const routes: Routes = [{ path: '', component: MainComponent }];
 
 @NgModule({
   declarations: [MainComponent],
   imports: [RouterModule.forChild(routes), CommonModule],
-  exports: [MainComponent]
+  providers: [
+    GetAlbumUseCases,
+    { provide: AlbumGateway, useClass: AlbumApiServiceWithoutDelay },
+    // { provide: AlbumGateway, useClass: AlbumApiService },
+  ],
+  exports: [MainComponent],
 })
 export class MainModule {}
--- angular-mf-workspace/projects/clean-architecture/src/app/app.module.ts
+++ angular-mf-workspace/projects/clean-architecture/src/app/app.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { AlbumGateway } from './domain/models/Album/gateway/album-gateway';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-// import { AlbumApiService } from './infraestructure/driven-adapter/album-api/album-api.service';
-import { AlbumApiServiceWithoutDelay } from './infraestructure/driven-adapter/album-api/album-api-withou-delay.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
-  providers: [
-    { provide: AlbumGateway, useClass: AlbumApiServiceWithoutDelay },
-    /* { provide: AlbumGateway, useClass: AlbumApiService }, */
-  ],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
